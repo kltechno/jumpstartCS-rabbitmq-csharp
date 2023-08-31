cf09f2b3f5b8dbe03c327a40b2e238d8631ac5b6
kltechno <58072931+kltechno@users.noreply.github.com>
2023-08-31 11:19:15 +0100

    Add narration to pub/sub

--- 3-Pub-Sub/FirstConsumer/Program.cs
+++ 3-Pub-Sub/FirstConsumer/Program.cs
@@ -3,18 +3,23 @@ using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
+// 1 Create a connection to RabbitMQ using ConnectionFactory class.
 var factory = new ConnectionFactory() { HostName = "localhost" };
 
 using var connection = factory.CreateConnection();
 
+// 2 Create a channel to communicate with RabbitMQ. (English Channel)
 using var channel = connection.CreateModel();
 
+// 3 Declare an exchange of type Fanout with name pubsut  (Landing craft exchanges)
 channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
 
+// 4 Declare a queue and binds to pubsub with an empty routing key. (Queues of food can)
 var queueName = channel.QueueDeclare().QueueName;
 
 channel.QueueBind(queue: queueName, exchange: "pubsub", routingKey: "");
 
+// 5 Create a consumer and setup the event handler to convert message body to a string. (Consumers on beach)
 var consumer = new EventingBasicConsumer(channel);
 
 consumer.Received += (model, ea) =>
@@ -24,6 +29,7 @@ consumer.Received += (model, ea) =>
     Console.WriteLine($"FirstConsumer - Recieved new message: {message}");
 };
 
+// 6 Start consuming messages from the queue with auto acknowledgement enabled. (Consumers running towards queues of cans)
 channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
 Console.WriteLine("Consuming");
--- 3-Pub-Sub/Producer/Program.cs
+++ 3-Pub-Sub/Producer/Program.cs
@@ -2,18 +2,23 @@
 using System.Text;
 using RabbitMQ.Client;
 
+// 1 Create a connection to RabbitMQ using ConnectionFactory class.
 var factory = new ConnectionFactory() { HostName = "localhost" };
 
 using var connection = factory.CreateConnection();
 
+// 2 Create a channel to communicate with RabbitMQ. (English Channel)
 using var channel = connection.CreateModel();
 
+// 3 Declare an exchange of type Fanout with name pubsut  (Landing craft exchanges)
 channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
 
 var message = "Hello I want to broadcast this message";
 
+// 4 Convert your msg to byte array
 var body = Encoding.UTF8.GetBytes(message);
 
+// 5 Publish to exchange with an empty routing key, and no properties.
 channel.BasicPublish(exchange: "pubsub", "", null, body);
 
 Console.WriteLine($"Send message: {message}");
